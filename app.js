9d8e860533681ca77038ba69d83b4f293c3b3d3b
Sean Yu <canvaslike@gmail.com>
2020-10-27 14:27:21 +0800

    feat: project in

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(function (req, res, next) {
+  const dateNow = new Date(Date.now()).toLocaleString('en-US', { hour12: false })
+  console.log(dateNow, '|', req.method, 'from', req.originalUrl)
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('列出全部 Todo')
+})
+
+app.get('/new', (req, res) => {
+  res.send('新增 Todo 頁面')
+})
+
+app.get('/:id', (req, res) => {
+  res.send('顯示一筆 Todo')
+})
+
+app.post('/', (req, res) => {
+  res.send('新增一筆  Todo')
+})
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`)
+})
